8126ecb6655ca715ea4e3e4993b3d3a74771ca4d
Jesus Herrera <jesus3846@hotmail.com>
2020-02-26 16:07:13 -0500

    Avance de registro de usuario

--- app/src/main/java/com/example/prueba/Registro.java
+++ app/src/main/java/com/example/prueba/Registro.java
@@ -2,13 +2,24 @@ package com.example.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registro extends AppCompatActivity {
-
+    Button reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        reg=(Button) findViewById(R.id.registrarse);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Registrarse.class);
+                startActivity(i);
+            }
+        });
     }
 }
