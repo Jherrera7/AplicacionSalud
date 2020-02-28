b436be8965a219944d3aec4d22d85900aed2bb6f
Jesus Herrera <jesus3846@hotmail.com>
2020-02-27 20:20:58 -0500

    Avance con registro fisico

--- app/src/main/java/com/example/prueba/Registrarse.java
+++ app/src/main/java/com/example/prueba/Registrarse.java
@@ -2,13 +2,25 @@ package com.example.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registrarse extends AppCompatActivity {
-
+    Button reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+        reg=(Button)findViewById(R.id.cargar);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Rgistrofisico.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
